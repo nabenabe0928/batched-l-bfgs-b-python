f5cbd4b50338a369d8391fef54a8a44eaeb32415
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-08-18 04:18:57 +0200

    License

--- batched_lbfgsb.py
+++ batched_lbfgsb.py
@@ -1,27 +1,30 @@
-## License for the Python wrapper
-## ==============================
+"""
+Please note that this is a port of the L-BFGS-B iplementation from SciPy.
 
-## Copyright (c) 2004 David M. Cooke <cookedm@physics.mcmaster.ca>
+Significantly modified to adapt to Optuna by Shuhei Watanabe (2025) <shuheiwatanabe@preferred.jp>
 
-## Permission is hereby granted, free of charge, to any person obtaining a
-## copy of this software and associated documentation files (the "Software"),
-## to deal in the Software without restriction, including without limitation
-## the rights to use, copy, modify, merge, publish, distribute, sublicense,
-## and/or sell copies of the Software, and to permit persons to whom the
-## Software is furnished to do so, subject to the following conditions:
+License for the Python wrapper
+==============================
 
-## The above copyright notice and this permission notice shall be included in
-## all copies or substantial portions of the Software.
+Copyright (c) 2004 David M. Cooke <cookedm@physics.mcmaster.ca>
 
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-## DEALINGS IN THE SOFTWARE.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-## Modifications by Travis Oliphant and Enthought, Inc. for inclusion in SciPy
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Modifications by Travis Oliphant and Enthought, Inc. for inclusion in SciPy
+"""
 
 from __future__ import annotations
 
@@ -31,6 +34,7 @@ import numpy as np
 from packaging.version import Version
 from scipy import __version__ as scipy_version
 from scipy.optimize import _lbfgsb as scipy_lbfgsb
+
 
 if TYPE_CHECKING:
     from typing import Any
@@ -199,13 +203,14 @@ class _TaskStatusManager:
             return True
         if self._judge_which_status(batch_id, status_id=1):  # New parameter suggested.
             self._n_iterations[batch_id] += 1
-            self.is_batch_terminated[batch_id] |= (
-                self.reach_iteration_limit(batch_id) or self.reach_evaluation_limit(batch_id)
-            )
-        elif (
-            not self._judge_which_status(batch_id, status_id=0)  # Start
-            and not self._judge_which_status(batch_id, status_id=3)  # Next function evaluation
-        ):
+            self.is_batch_terminated[batch_id] |= self.reach_iteration_limit(
+                batch_id
+            ) or self.reach_evaluation_limit(batch_id)
+        elif not self._judge_which_status(
+            batch_id, status_id=0
+        ) and not self._judge_which_status(  # Start
+            batch_id, status_id=3
+        ):  # Next function evaluation
             self.is_batch_terminated[batch_id] = True
         return self.is_batch_terminated[batch_id]
 
